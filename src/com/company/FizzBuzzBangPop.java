953fc5ccc92fbbf8a85127900e5d351ca804b1e5
Max Roberts-Dear <mrobertsdear@uk-c02yv2lalvdm.uk.deloitte.com>
2019-10-09 14:33:33 +0100

    convert(primeNumber) returns Bang

--- src/com/company/FizzBuzzBangPop.java
+++ src/com/company/FizzBuzzBangPop.java
@@ -3,12 +3,33 @@ package com.company;
 public class FizzBuzzBangPop {
 
     public static String convert(int input) {
-        if (input % 15 == 0) {
+        if (FizzBuzzBangPop.checkPrime(input)) {
+            return "Bang";
+        }
+        else if (input % 15 == 0) {
             return "FizzBuzz";
-        } else if (input % 5 == 0){
+        } else if (input % 5 == 0) {
             return "Buzz";
         } else {
             return "Fizz";
+        }
+    }
+
+    private static boolean checkPrime(int n) {
+        int i, m = 0, flag = 0;
+        m = n / 2;
+        if (n == 0 || n == 1) {
+            return false;
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            if (flag == 0) {
+                return true;
+            }
+            return true;
         }
     }
 
--- src/com/company/FizzBuzzBangPopTest.java
+++ src/com/company/FizzBuzzBangPopTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class FizzBuzzBangPopTest {
 
     @Test
-    void convert_Fizz() {
-        assertEquals("Fizz", FizzBuzzBangPop.convert(3));
+    void convert_3_Fizz() {
+        assertEquals("Fizz", FizzBuzzBangPop.convert(27));
         assertEquals("Fizz", FizzBuzzBangPop.convert(6));
         assertEquals("Fizz", FizzBuzzBangPop.convert(9));
         assertEquals("Fizz", FizzBuzzBangPop.convert(33));
@@ -16,8 +16,8 @@ class FizzBuzzBangPopTest {
     }
 
     @Test
-    void convert_Buzz() {
-        assertEquals("Buzz", FizzBuzzBangPop.convert(5));
+    void convert_5_Buzz() {
+        assertEquals("Buzz", FizzBuzzBangPop.convert(500));
         assertEquals("Buzz", FizzBuzzBangPop.convert(10));
         assertEquals("Buzz", FizzBuzzBangPop.convert(50));
         assertEquals("Buzz", FizzBuzzBangPop.convert(55));
@@ -25,12 +25,21 @@ class FizzBuzzBangPopTest {
     }
 
     @Test
-    void convert_FizzBuzz() {
+    void convert_15_FizzBuzz() {
         assertEquals("FizzBuzz", FizzBuzzBangPop.convert(15));
         assertEquals("FizzBuzz", FizzBuzzBangPop.convert(30));
         assertEquals("FizzBuzz", FizzBuzzBangPop.convert(150));
         assertEquals("FizzBuzz", FizzBuzzBangPop.convert(45));
         assertEquals("FizzBuzz", FizzBuzzBangPop.convert(60));
+    }
+
+    @Test
+    void convert_prime_Bang() {
+        assertEquals("Bang", FizzBuzzBangPop.convert(3));
+        assertEquals("Bang", FizzBuzzBangPop.convert(17));
+        assertEquals("Bang", FizzBuzzBangPop.convert(101));
+        assertEquals("Bang", FizzBuzzBangPop.convert(139));
+        assertEquals("Bang", FizzBuzzBangPop.convert(191));
     }
 
 
