fc29f73c8e69592dfc9d589d3dbb2a03b0fdde55
Max Roberts-Dear <mrobertsdear@uk-c02yv2lalvdm.uk.deloitte.com>
2019-10-09 14:19:07 +0100

    Added tests for multiple cases Refactored functional code to pass failing tests

--- src/com/company/FizzBuzzBangPop.java
+++ src/com/company/FizzBuzzBangPop.java
@@ -3,9 +3,9 @@ package com.company;
 public class FizzBuzzBangPop {
 
     public static String convert(int input) {
-        if (input == 15) {
+        if (input % 15 == 0) {
             return "FizzBuzz";
-        } else if (input == 5){
+        } else if (input % 5 == 0){
             return "Buzz";
         } else {
             return "Fizz";
--- src/com/company/FizzBuzzBangPopTest.java
+++ src/com/company/FizzBuzzBangPopTest.java
@@ -9,16 +9,28 @@ class FizzBuzzBangPopTest {
     @Test
     void convert_Fizz() {
         assertEquals("Fizz", FizzBuzzBangPop.convert(3));
+        assertEquals("Fizz", FizzBuzzBangPop.convert(6));
+        assertEquals("Fizz", FizzBuzzBangPop.convert(9));
+        assertEquals("Fizz", FizzBuzzBangPop.convert(33));
+        assertEquals("Fizz", FizzBuzzBangPop.convert(36));
     }
 
     @Test
     void convert_Buzz() {
         assertEquals("Buzz", FizzBuzzBangPop.convert(5));
+        assertEquals("Buzz", FizzBuzzBangPop.convert(10));
+        assertEquals("Buzz", FizzBuzzBangPop.convert(50));
+        assertEquals("Buzz", FizzBuzzBangPop.convert(55));
+        assertEquals("Buzz", FizzBuzzBangPop.convert(65));
     }
 
     @Test
     void convert_FizzBuzz() {
         assertEquals("FizzBuzz", FizzBuzzBangPop.convert(15));
+        assertEquals("FizzBuzz", FizzBuzzBangPop.convert(30));
+        assertEquals("FizzBuzz", FizzBuzzBangPop.convert(150));
+        assertEquals("FizzBuzz", FizzBuzzBangPop.convert(45));
+        assertEquals("FizzBuzz", FizzBuzzBangPop.convert(60));
     }
 
 
